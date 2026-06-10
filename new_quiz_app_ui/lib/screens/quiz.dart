4c877510b6c7747815383b14e213de1dc25a092d
TanvirIshrak <tanvirishrak2963@gmail.com>
2026-06-10 10:36:50 +0600

    assignment part complete

--- new_quiz_app_ui/lib/screens/quiz.dart
+++ new_quiz_app_ui/lib/screens/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
@@ -9,87 +10,91 @@ class Quiz extends StatefulWidget {
 class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: Text('Quiz'),),
+    return Scaffold(
+      appBar: AppBar(title: Text('Quiz')),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 23.0),
         child: Container(
           child: Column(
             crossAxisAlignment: .start,
             children: [
-              SizedBox(height: 20,),
-              Text('Question 1/10', style: TextStyle(fontWeight: .w600, fontSize: 20),),
-
-              SizedBox(height: 20,),
-              Flexible(child: Text('What is Flutter?', style: TextStyle(fontWeight: .w600, fontSize: 30),)),
-
-              Expanded(
-                  child: ListView(
-                    children: [
-                      Card(
-                        child: ListTile(
-                          title: Text('Toolkit'),
-                          leading: Radio<int>(value: 1,),
-                          trailing: Icon(Icons.info),
-                        ),
-                      ),
-                      Card(
-                        child: ListTile(
-                          title: Text('Database'),
-                          leading: Radio<int>(value: 1,),
-                          trailing: Icon(Icons.info),
-                        ),
-                      ),
-                      Card(
-                        child: ListTile(
-                          title: Text('Language'),
-                          leading: Radio<int>(value: 1,),
-                          trailing: Icon(Icons.info),
-                        ),
-                      ),
-                      Card(
-                        child: ListTile(
-                          title: Text('IDE'),
-                          leading: Radio<int>(value: 1,),
-                          trailing: Icon(Icons.info),
-                        ),
-                      ),
-                    ],
-                  )
+              SizedBox(height: 20),
+              Text(
+                'Question 1/10',
+                style: TextStyle(fontWeight: .w600, fontSize: 20),
               ),
 
-              Spacer(),
-              Row(
-                mainAxisAlignment: .center,
-                children: [
-                  Expanded(
-                    child: SizedBox(
-                        height: 60,
-                        child: ElevatedButton(
-                            onPressed: (){},
-                            child: Text('Previous')
-                        )
-                    ),
-                  ),
+              SizedBox(height: 20),
+              Flexible(
+                child: Text(
+                  'What is Flutter?',
+                  style: TextStyle(fontWeight: .w600, fontSize: 30),
+                ),
+              ),
 
-                  SizedBox(width: 20,),
-                  Expanded(
-                    child: SizedBox(
-                        height: 60,
-                        child: ElevatedButton(
-                            onPressed: (){},
-                            child: Text('Next')
-                        )
+              Expanded(
+                child: ListView(
+                  children: [
+                    Card(
+                      child: ListTile(
+                        title: Text('Toolkit'),
+                        leading: Radio<int>(value: 1),
+                        trailing: Icon(Icons.info),
+                      ),
                     ),
-                  ),
-                ],
-              )
-
+                    Card(
+                      child: ListTile(
+                        title: Text('Database'),
+                        leading: Radio<int>(value: 1),
+                        trailing: Icon(Icons.info),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        title: Text('Language'),
+                        leading: Radio<int>(value: 1),
+                        trailing: Icon(Icons.info),
+                      ),
+                    ),
+                    Card(
+                      child: ListTile(
+                        title: Text('IDE'),
+                        leading: Radio<int>(value: 1),
+                        trailing: Icon(Icons.info),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       ),
-    );
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 28.0, horizontal: 25),
+        child: Row(
+          mainAxisAlignment: .center,
+          children: [
+            Expanded(
+              child: SizedBox(
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Previous'),
+                ),
+              ),
+            ),
 
+            SizedBox(width: 20),
+            Expanded(
+              child: SizedBox(
+                height: 60,
+                child: ElevatedButton(onPressed: () {}, child: Text('Next')),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
