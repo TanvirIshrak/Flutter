635bf347e061e5271ad0d61353e54ef13e3b82f4
TanvirIshrak <tanvirishrak2963@gmail.com>
2026-08-11 05:07:04 +0600

    book details page done

---
+++ jamat_e_islami_books_store/lib/pages/BookDetails/BookActionButton.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:jamat_e_islami_books_store/pages/BookPage/BookPage.dart';
+class BookActionButton extends StatelessWidget {
+  const BookActionButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(20)
+      ),
+      child: Row(
+        mainAxisAlignment: .spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> BookPage()));
+              },
+              child: Row(
+                children: [
+                  SvgPicture.asset("Assets/Icons/book.svg"),
+
+                  SizedBox(width: 20,),
+                  Text("Read Book", style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.background),
+                  )
+
+                ],
+              ),
+            ),
+          ),
+          Container(
+            width: 2,
+            height: 30,
+            color: Theme.of(context).colorScheme.background,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Row(
+              children: [
+                SvgPicture.asset("Assets/Icons/play.svg"),
+                SizedBox(width: 20,),
+                Text("Play Book", style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.background),
+                )
+
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- jamat_e_islami_books_store/lib/pages/BookDetails/BookDetails.dart
+++ jamat_e_islami_books_store/lib/pages/BookDetails/BookDetails.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jamat_e_islami_books_store/components/AppBackButton.dart';
 import 'package:jamat_e_islami_books_store/config/colors.dart';
+import 'package:jamat_e_islami_books_store/pages/BookDetails/BookActionButton.dart';
 import 'package:jamat_e_islami_books_store/pages/BookDetails/Header.dart';
 class BookDetails extends StatelessWidget {
   const BookDetails({super.key});
@@ -9,23 +10,68 @@ class BookDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            height: 550,
-            color: Theme.of(context).colorScheme.primary,
-            child: Row(
-              children: [
-                Expanded(
-                  // lib / pages/ BookDetails/ Header.dart
-                  child: HeaderDetails()
-                )
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              height: 550,
+              color: Theme.of(context).colorScheme.primary,
+              child: Row(
+                children: [
+                  Expanded(
+                    // lib / pages/ BookDetails/ Header.dart
+                    child: HeaderDetails()
+                  )
 
-              ],
+                ],
+              ),
             ),
-          )
-        ],
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("About Book", style: Theme.of(context).textTheme.bodyMedium,),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Text("মূলত রাজনৈতিক ও ঐতিহাসিক প্রেক্ষাপটে রচিত একটি পরিচিত বই, যার লেখক জামায়াতে ইসলামীর সাবেক আমীর অধ্যাপক গোলাম আযম।",
+                          style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Text("About Author", style: Theme.of(context).textTheme.bodyMedium,),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Text("লেখক জামায়াতে ইসলামীর সাবেক আমীর অধ্যাপক গোলাম আযম।",
+                          style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30,),
+
+                  // lib/pages/BookDetails/BookActionButton.dart
+                  BookActionButton()
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
